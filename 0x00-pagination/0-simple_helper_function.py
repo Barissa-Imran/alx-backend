8d8dc8938b0821001517dada0c59b72159fbd839
Barissa-Imran <imranbarissa@gmail.com>
2023-08-21 22:39:38 +0300

    add 1. Simple pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """API Design: Pagination module"""
 import csv
-from typing import Tuple
+import math
+from typing import Tuple, List
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -12,14 +13,3 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
-
-
-class Server:
-    """
-    Server class to paginate a db of popular baby names.
-    """
-    DATA_FILE = "Popular_Baby_Names.csv"
-
-    def __init__(self) -> None:
-        """instantiate a new server instance"""
-        self._dataset = None
