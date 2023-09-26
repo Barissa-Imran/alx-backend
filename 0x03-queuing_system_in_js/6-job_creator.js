5c1b7f3848e156911c4bcd33f6b67f643f537cbd
Barissa-Imran <imranbarissa@gmail.com>
2023-09-26 23:10:19 +0300

    7. Create the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,8 +2,8 @@ const kue = require('kue');
 const queue = kue.createQueue();
 
 const JobData = {
-  phoneNumber: string,
-  message: string,
+  phoneNumber: '4153518780',
+  message: 'This is code to verify account',
 };
 
 const job = queue.create('push_notification_code', JobData).save((error) => {
